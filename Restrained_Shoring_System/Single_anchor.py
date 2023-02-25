37af55b2cb6f771c051e05b7754c18927f2eed91
ali <ali.s.tag2000@gmail.com>
2023-02-25 09:39:38 +0330

    add water. need to be checked.

--- Restrained_Shoring_System/Single_anchor.py
+++ Restrained_Shoring_System/Single_anchor.py
@@ -11,7 +11,8 @@ from pressure import force_calculator, force_calculator_x, find_D, edit_sigma_an
 
 def single_anchor(spacing, FS, h1, h, trapezoidal_force, trapezoidal_force_arm, surcharge_force, surcharge_arm,
                   force_active, arm_active, force_passive,
-                  arm_passive, sigma_active, sigma_passive, delta_h):
+                  arm_passive, sigma_active, sigma_passive, water_force_active, water_arm_active, water_force_passive,
+                  water_arm_passive, water_started, delta_h):
     D = symbols("D")
     driving_force = []
     for i in force_active:
@@ -39,10 +40,56 @@ def single_anchor(spacing, FS, h1, h, trapezoidal_force, trapezoidal_force_arm, 
     if delta_h_decimal == -1:
         delta_h_decimal = 0
 
-    d = find_D(FS, resisting_force, resisting_force_arm, driving_force, driving_force_arm)
-    d_0 = find_D(1, resisting_force, resisting_force_arm, driving_force, driving_force_arm)
-    d = round(d, delta_h_decimal)
-    d_0 = round(d_0, delta_h_decimal)
+    if water_started > h:
+        d = find_D(FS, resisting_force, resisting_force_arm, driving_force, driving_force_arm)
+        d_0 = find_D(1, resisting_force, resisting_force_arm, driving_force, driving_force_arm)
+        d = round(d, delta_h_decimal)
+        d_0 = round(d_0, delta_h_decimal)
+        if d > water_started - h:
+            # add water pressure now is necessary
+            # first I should consider D from water started - h
+            for item in [driving_force, resisting_force, driving_force_arm, resisting_force_arm]:
+                for i in range(len(item)):
+                    if type(item[i]) == sympy.core.mul.Mul or type(item[i]) == sympy.core.add.Add:
+                        item[i] = item[i].subs(D, D + (water_started - h))
+            # now I should add water pressure
+            for i in water_force_active:
+                for j in i:
+                    driving_force.append(j)
+            for i in water_force_passive:
+                for j in i:
+                    resisting_force.append(j)
+            for i in water_arm_active:
+                for j in i:
+                    driving_force_arm.append(j)
+            for i in water_arm_passive:
+                for j in i:
+                    resisting_force_arm.append(j)
+
+        d = find_D(FS, resisting_force, resisting_force_arm, driving_force, driving_force_arm)
+        d_0 = find_D(1, resisting_force, resisting_force_arm, driving_force, driving_force_arm)
+        d = round(d + (water_started - h), delta_h_decimal)
+        d_0 = round(d_0 + water_started - h, delta_h_decimal)
+
+    else:
+        # add water pressure
+        for i in water_force_active:
+            for j in i:
+                driving_force.append(j)
+        for i in water_force_passive:
+            for j in i:
+                resisting_force.append(j)
+        for i in water_arm_active:
+            for j in i:
+                driving_force_arm.append(j)
+        for i in water_arm_passive:
+            for j in i:
+                resisting_force_arm.append(j)
+        d = find_D(FS, resisting_force, resisting_force_arm, driving_force, driving_force_arm)
+        d_0 = find_D(1, resisting_force, resisting_force_arm, driving_force, driving_force_arm)
+        d = round(d, delta_h_decimal)
+        d_0 = round(d_0, delta_h_decimal)
+
     # replace d0 in D for all values
     for item in [sigma_active, sigma_passive, resisting_force, resisting_force_arm, driving_force,
                  driving_force_arm]:
