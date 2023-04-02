439e81d08c5eb97f541d89de4e4f9eb958a2550c
ali <ali.s.tag2000@gmail.com>
2023-04-02 13:50:54 +0430

    add first site inpus.

---
+++ Restrained_Shoring_System/site_input.py
@@ -0,0 +1,26 @@
+site_input = {'information': {'title': 'title', 'jobNo': 'job No', 'designer': 'designer', 'checker': 'checker',
+                              'company': 'company', 'client': 'client', 'unit': 'us', 'date': None, 'comment': None,
+                              'other': None}, 'product_id': 27, 'user_id': 44, 'data': {
+    'General Properties': {'FS': {'value': '1.5', 'unit': None}, 'E': {'value': '29000', 'unit': 'ksi'},
+                           'Pile Spacing': {'value': '6', 'unit': 'ft'}, 'Fy': {'value': '36', 'unit': 'ksi'},
+                           'Allowable Deflection': {'value': '0.5', 'unit': 'in'},
+                           'Sections': {'value': '{"id":1371,"item":"W40,W36,W33","section_product_item":"44_86"}',
+                                        'unit': None}},
+    'Geometrical Properties': {'Retaining Height': {'value': '10', 'unit': 'ft'},
+                               'Number of Anchors': {'value': '{"id":1472,"item":"1","section_product_item":"45_96"}',
+                                                     'unit': None}, 'Angle of Anchors': {'value': '0', 'unit': '°'},
+                               'h1': {'value': '4', 'unit': 'ft'}}, 'Soil Properties': {
+        'Formula': {'value': '{"id":1488,"item":"Rankine","section_product_item":"46_97"}', 'unit': None},
+        'γ': {'value': '125', 'unit': 'pcf'}, 'Φ': {'value': '34', 'unit': '°'},
+        'β Active': {'value': '0', 'unit': '°'}, 'β Passive': {'value': '0', 'unit': '°'},
+        'Soil Cohesion': {'value': '{"id":1538,"item":"Cohesionless","section_product_item":"46_97"}', 'unit': None}},
+    'Water Properties': {
+        'There is water?': {'value': '{"id":1541,"item":"No","section_product_item":"47_99"}', 'unit': None}},
+    'Surcharge': {
+        'Load Type': {'value': '{"id":1297,"item":"Point Load","section_product_item":"48_87"}', 'unit': None},
+        'Load Type ': {'value': '{"id":1305,"item":"No Load","section_product_item":"48_87"}', 'unit': None},
+        'Load Type  ': {'value': '{"id":1312,"item":"No Load","section_product_item":"48_87"}', 'unit': None},
+        'Load Type   ': {'value': '{"id":1314,"item":"No Load","section_product_item":"48_87"}', 'unit': None}},
+    'Lagging': {'Ph max': {'value': '100', 'unit': 'psf'}, 'Fb': {'value': '25', 'unit': 'ksi'},
+                'Timber Size': {'value': '{"id":1403,"item":"2 x 10","section_product_item":"49_93"}', 'unit': None}}},
+              'type': 1, 'number_of_projects': 1}
