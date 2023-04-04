4a79e95302c0d07fda68b46cc318dfacb2add23c
ali <ali.s.tag2000@gmail.com>
2023-04-04 15:02:03 +0430

    add new input.

--- Restrained_Shoring_System/site_input.py
+++ Restrained_Shoring_System/site_input.py
@@ -24,9 +24,9 @@ site_input2 = {'information': {'title': 'title', 'jobNo': 'job No', 'designer': 
     'Lagging': {'Ph max': {'value': '100', 'unit': 'psf'}, 'Fb': {'value': '25', 'unit': 'ksi'},
                 'Timber Size': {'value': '{"id":1403,"item":"2 x 10","section_product_item":"49_93"}', 'unit': None}}},
                'type': 1, 'number_of_projects': 1}
-site_input = {'information': {'title': 'title', 'jobNo': 'job No', 'designer': 'designer', 'checker': 'checker',
-                              'company': 'company', 'client': 'client', 'unit': 'us', 'date': None, 'comment': None,
-                              'other': None}, 'product_id': 27, 'user_id': 44, 'data': {
+site_input3 = {'information': {'title': 'title', 'jobNo': 'job No', 'designer': 'designer', 'checker': 'checker',
+                               'company': 'company', 'client': 'client', 'unit': 'us', 'date': None, 'comment': None,
+                               'other': None}, 'product_id': 27, 'user_id': 44, 'data': {
     'General Properties': {'FS': {'value': '1.5', 'unit': None}, 'E': {'value': '29000', 'unit': 'ksi'},
                            'Pile Spacing': {'value': '6', 'unit': 'ft'}, 'Fy': {'value': '36', 'unit': 'ksi'},
                            'Allowable Deflection': {'value': '0.5', 'unit': 'in'},
@@ -56,11 +56,11 @@ site_input = {'information': {'title': 'title', 'jobNo': 'job No', 'designer': '
                   'L1  ': {'value': '5', 'unit': 'ft'}},
     'Lagging': {'Ph max': {'value': '100', 'unit': 'psf'}, 'Fb': {'value': '25', 'unit': 'ksi'},
                 'Timber Size': {'value': '{"id":1403,"item":"1 x 2","section_product_item":"49_93"}', 'unit': None}}},
-              'type': 1, 'number_of_projects': 1}  # output is not rational! negative embedment.
+               'type': 1, 'number_of_projects': 1}  # output is not rational! negative embedment.
 
-site_input3 = {'information': {'title': 'title', 'jobNo': 'job No', 'designer': 'designer', 'checker': 'checker',
-                              'company': 'company', 'client': 'client', 'unit': 'us', 'date': None, 'comment': None,
-                              'other': None}, 'product_id': 27, 'user_id': 44, 'data': {
+site_input4 = {'information': {'title': 'title', 'jobNo': 'job No', 'designer': 'designer', 'checker': 'checker',
+                               'company': 'company', 'client': 'client', 'unit': 'us', 'date': None, 'comment': None,
+                               'other': None}, 'product_id': 27, 'user_id': 44, 'data': {
     'General Properties': {'FS': {'value': '1.5', 'unit': None}, 'E': {'value': '29000', 'unit': 'ksi'},
                            'Pile Spacing': {'value': '6', 'unit': 'ft'}, 'Fy': {'value': '36', 'unit': 'ksi'},
                            'Allowable Deflection': {'value': '0.5', 'unit': 'in'},
@@ -90,4 +90,34 @@ site_input3 = {'information': {'title': 'title', 'jobNo': 'job No', 'designer': 
                   'L1  ': {'value': '5', 'unit': 'ft'}},
     'Lagging': {'Ph max': {'value': '100', 'unit': 'psf'}, 'Fb': {'value': '25', 'unit': 'ksi'},
                 'Timber Size': {'value': '{"id":1403,"item":"1 x 2","section_product_item":"49_93"}', 'unit': None}}},
-              'type': 1, 'number_of_projects': 1}  # there is error! figure it out.
+               'type': 1, 'number_of_projects': 1}  # there is error! figure it out.
+
+site_input = {
+    'information': {'title': '1', 'jobNo': '1', 'designer': '11', 'checker': '1', 'company': '1', 'client': '1',
+                    'unit': 'us', 'date': None, 'comment': None, 'other': None}, 'product_id': 27, 'user_id': 44,
+    'data': {'General Properties': {'FS': {'value': '1.5', 'unit': None}, 'E': {'value': '29000', 'unit': 'ksi'},
+                                    'Pile Spacing': {'value': '6', 'unit': 'ft'}, 'Fy': {'value': '36', 'unit': 'ksi'},
+                                    'Allowable Deflection': {'value': '0.5', 'unit': 'in'},
+                                    'Sections': {'value': '{"id":1371,"item":"W44,W40","section_product_item":"44_86"}',
+                                                 'unit': None}},
+             'Geometrical Properties': {'Retaining Height': {'value': '10', 'unit': 'ft'}, 'Number of Anchors': {
+                 'value': '{"id":1472,"item":"4","section_product_item":"45_96"}', 'unit': None},
+                                        'Angle of Anchors': {'value': '0', 'unit': '°'},
+                                        'h1': {'value': '4', 'unit': 'ft'}, 'h2': {'value': '1', 'unit': 'ft'},
+                                        'h3': {'value': '1', 'unit': 'ft'}, 'h4': {'value': '1', 'unit': 'ft'}},
+             'Soil Properties': {
+                 'Formula': {'value': '{"id":1488,"item":"Rankine","section_product_item":"46_97"}', 'unit': None},
+                 'γ': {'value': '120', 'unit': 'pcf'}, 'Φ': {'value': '34', 'unit': '°'},
+                 'β Active': {'value': '0', 'unit': '°'}, 'β Passive': {'value': '0', 'unit': '°'},
+                 'Soil Cohesion': {'value': '{"id":1538,"item":"Cohesionless","section_product_item":"46_97"}',
+                                   'unit': None}}, 'Water Properties': {
+            'There is water?': {'value': '{"id":1541,"item":"No","section_product_item":"47_99"}', 'unit': None}},
+             'Surcharge': {
+                 'Load Type': {'value': '{"id":1381,"item":"No Load","section_product_item":"48_92"}', 'unit': None},
+                 'Load Type ': {'value': '{"id":1382,"item":"No Load","section_product_item":"48_92"}', 'unit': None},
+                 'Load Type  ': {'value': '{"id":1383,"item":"No Load","section_product_item":"48_92"}', 'unit': None},
+                 'Load Type   ': {'value': '{"id":1384,"item":"No Load","section_product_item":"48_92"}',
+                                  'unit': None}},
+             'Lagging': {'Ph max': {'value': '120', 'unit': 'psf'}, 'Fb': {'value': '120', 'unit': 'ksi'},
+                         'Timber Size': {'value': '{"id":1403,"item":"1 x 2","section_product_item":"49_93"}',
+                                         'unit': None}}}, 'type': 1, 'number_of_projects': 1}
